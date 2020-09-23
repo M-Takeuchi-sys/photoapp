d16819d551cf9af22e0ccdbb69fe8e13fdcc22d5
masao <>
2020-09-24 08:07:46 +0900

    投稿フォームレイアウト完成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -47,4 +47,35 @@ document.addEventListener('DOMContentLoaded', () => {
     reader.readAsDataURL(imageFile);
     return def.promise();
   };
+
+  $('#formHeaderPost').on('click', () => {
+    $('#submitBtn').trigger("click");
+  })
+
+  $('#imageInputBtn').on('click', () => {
+    $('#imagesBtn').trigger("click");
+  })
+
+  $("#imagesBtn").on("change", function (e) {
+    var files = e.target.files;
+    var d = new $.Deferred().resolve();
+    $.each(files, function (i, file) {
+      d = d.then(function () {
+        return previewImage(file);
+      });
+    });
+  });
+
+  var previewImage = function (imageFile) {
+    var reader = new FileReader();
+    var img = new Image();
+    var def = $.Deferred();
+    reader.onload = function (e) {
+      $("#imagesPreview").append(img);
+      img.src = e.target.result;
+      def.resolve(img);
+    };
+    reader.readAsDataURL(imageFile);
+    return def.promise();
+  };
 })
