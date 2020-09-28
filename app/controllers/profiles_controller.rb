290ec6c60871513be8611ecde5d689e5c2720420
masao <>
2020-09-28 17:57:50 +0900

    profileパラメーターがない時でも保存アクションできるように設定

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -22,7 +22,7 @@ class ProfilesController < ApplicationController
 
   private
   def profile_params
-    params.require(:profile).permit(
+    params.fetch(:profile, {}).permit( #prlfileパラメータがない時は {} がデフォルト値
       :avatar
     )
   end
