e2793ba8691ea5d1c990f8fc43601762d3987c7f
masao <>
2020-10-02 21:58:40 +0900

    childrenn削除

--- app/javascript/packs/photo.js
+++ app/javascript/packs/photo.js
@@ -9,26 +9,27 @@ const photoId = dataset.photoId
 
 const handleHeartDisplay = (hasLiked) => {
   if (hasLiked) {
-    $(`.active-heart${photoId}`).children().removeClass('hidden')
+    $(`.active-heart${photoId}`).removeClass('hidden')
   } else {
-    $(`.inactive-heart${photoId}`).children().removeClass('hidden')
+    $(`.inactive-heart${photoId}`).removeClass('hidden')
   }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-
+  const dataset = $('#photo-id').data()
+  const photoId = dataset.photoId
   axios.get(`/photos/${photoId}/like`)
     .then((response) => {
       const hasLiked = response.data.hasLiked
       handleHeartDisplay(hasLiked)
     })
 
-  $(`.inactive-heart${photoId}`).children().on('click', () => {
+  $(`.inactive-heart${photoId}`).on('click', () => {
     axios.post(`/photos/${photoId}/like`)
       .then((response) => {
         if (response.data.status === 'ok') {
-          $(`.active-heart${photoId}`).children().removeClass('hidden')
-          $(`.inactive-heart${photoId}`).children().addClass('hidden')
+          $(`.active-heart${photoId}`).removeClass('hidden')
+          $(`.inactive-heart${photoId}`).addClass('hidden')
         }
       })
       .catch((e) => {
@@ -37,12 +38,12 @@ document.addEventListener('DOMContentLoaded', () => {
       })
   })
 
-  $(`.active-heart${photoId}`).children().on('click', () => {
+  $(`.active-heart${photoId}`).on('click', () => {
     axios.delete(`/photos/${photoId}/like`)
       .then((response) => {
         if (response.data.status === 'ok') {
-          $(`.active-heart${photoId}`).children().addClass('hidden')
-          $(`.inactive-heart${photoId}`).children().removeClass('hidden')
+          $(`.active-heart${photoId}`).addClass('hidden')
+          $(`.inactive-heart${photoId}`).removeClass('hidden')
         }
       })
       .catch((e) => {
