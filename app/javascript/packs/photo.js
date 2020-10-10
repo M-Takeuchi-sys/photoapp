084bb73046b0272757d20e0b1c455d08443e6804
masao <>
2020-10-10 22:46:42 +0900

    $('.active-heart, #id').removeClass('hidden')の全てのphotoの表示が反映されてしまう

--- app/javascript/packs/photo.js
+++ app/javascript/packs/photo.js
@@ -8,20 +8,19 @@ document.addEventListener('DOMContentLoaded', () => {
   // var dataset = $('#photo-id').data()
   // var photoId = dataset.photoId
 
-  $('.heart-btn').on('click', (e) => {
-    e.preventDefault();
-    var id = $(e.currentTarget).attr('photo-id')
-    console.log(id);
-    axios.get(`/photos/18/like`)
-      .then((response) => {
-      // const hasLiked = response.data.hasLiked
-      //   if (hasLiked) {
-      //     $('.active-heart').removeClass('hidden')
-      //   } else {
-      //     $('.inactive-heart').removeClass('hidden')
-      //   }
-      })
-  })
+  // $('.heart-btn').on('click', function() {
+  //   var id = $(this).attr('photo-id')
+  //   console.log(id);
+  //   axios.get(`/photos/18/like`)
+  //     .then((response) => {
+  //     const hasLiked = response.data.hasLiked
+  //       if (hasLiked) {
+  //         $('.active-heart').removeClass('hidden')
+  //       } else {
+  //         $('.inactive-heart').removeClass('hidden')
+  //       }
+  //     })
+  // })
   
   // axios.get(`/photos/${photoId}/like`)
   //   .then((response) => {
@@ -33,37 +32,39 @@ document.addEventListener('DOMContentLoaded', () => {
   //     }
   //   })
 
-  // $('.inactive-heart').on('click', (e) => {
-  //   e.preventDefault();
-  //   var id = $(e.currentTarget).attr('id')
-  //   console.log(id);
-  //   axios.post(`/photos/${photoId}/like`)
-  //     .then((response) => {
-  //       if (response.data.status === 'ok') {
-  //         $('.active-heart',id).removeClass('hidden')
-  //         $('.inactive-heart',id).addClass('hidden')
-  //       }
-  //     })
-  //     .catch((e) => {
-  //       window.alert('Error')
-  //       console.log(e)
-  //     })
-  // })
+  $('.inactive-heart').on('click', (e) => {
+    e.preventDefault();
+    var id = $(e.currentTarget).attr('id')
+    console.log(id);
+    axios.post(`/photos/${id}/like`)
+      .then((response) => {
+        console.log(response)
+        if (response.data.status === 'ok') {
+          $('.active-heart, #id').removeClass('hidden')
+          $('.inactive-heart, #id').addClass('hidden')
+        }
+      })
+      .catch((e) => {
+        window.alert('Error')
+        console.log(e)
+      })
+  })
 
-  // $('.active-heart').on('click', (e) => {
-  //   e.preventDefault();
-  //   var id = $(e.currentTarget).attr('id')
-  //   console.log(id);
-  //   axios.delete(`/photos/${photoId}/like`)
-  //     .then((response) => {
-  //       if (response.data.status === 'ok') {
-  //         $('.active-heart',id).addClass('hidden')
-  //         $('.inactive-heart',id).removeClass('hidden')
-  //       }
-  //     })
-  //     .catch((e) => {
-  //       window.alert('Error')
-  //       console.log(e)
-  //     })
-  // })
+  $('.active-heart').on('click', (e) => {
+    e.preventDefault();
+    var id = $(e.currentTarget).attr('id')
+    console.log(id);
+    axios.delete(`/photos/${id}/like`)
+      .then((response) => {
+        console.log(response)
+        if (response.data.status === 'ok') {
+          $('.active-heart, #id').addClass('hidden')
+          $('.inactive-heart, #id').removeClass('hidden')
+        }
+      })
+      .catch((e) => {
+        window.alert('Error')
+        console.log(e)
+      })
+  })
 })
