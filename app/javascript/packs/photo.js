146d16918208a7cc78352f023e734f9d7cabccc7
masao <>
2020-10-12 23:01:41 +0900

    いいねのAJax完成

--- app/javascript/packs/photo.js
+++ app/javascript/packs/photo.js
@@ -5,44 +5,15 @@ import { csrfToken } from 'rails-ujs'
 axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
 
 document.addEventListener('DOMContentLoaded', () => {
-  // const dataset = $('#photo-id').data()
-  // const photoId = dataset.photoId
-  // console.log(photoId)
-
-  // $('.heart-btn').on('click', function() {
-  //   var id = $(this).attr('photo-id')
-  //   console.log(id);
-  //   axios.get(`/photos/18/like`)
-  //     .then((response) => {
-  //     const hasLiked = response.data.hasLiked
-  //       if (hasLiked) {
-  //         $('.active-heart').removeClass('hidden')
-  //       } else {
-  //         $('.inactive-heart').removeClass('hidden')
-  //       }
-  //     })
-  // })
-  
-  // axios.get(`/photos/${photoId}/like`)
-  //   .then((response) => {
-  //     const hasLiked = response.data.hasLiked
-  //     if (hasLiked) {
-  //       $('.active-heart').removeClass('hidden')
-  //     } else {
-  //       $('.inactive-heart').removeClass('hidden')
-  //     }
-  //   })
 
   $('.inactive-heart').on('click', (e) => {
     e.preventDefault();
-    var id = $(e.currentTarget).attr('id')
-    console.log(id);
+    var id = $(e.currentTarget).attr('id') //クリックしたdivのid取得
     axios.post(`/photos/${id}/like`)
       .then((response) => {
-        console.log(response)
         if (response.data.status === 'ok') {
           $(`.active-heart.${id}`).removeClass('hidden')
-          $(`.inactive-heart#${id}`).addClass('hidden')
+          $(`.inactive-heart.${id}`).addClass('hidden')
         }
       })
       .catch((e) => {
@@ -53,13 +24,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
   $('.active-heart').on('click', (e) => {
     e.preventDefault();
-    var id = $(e.currentTarget).attr('id')
-    console.log(id);
+    var id = $(e.currentTarget).attr('id') //クリックしたdivのid取得
     axios.delete(`/photos/${id}/like`)
       .then((response) => {
-        console.log(response)
         if (response.data.status === 'ok') {
-          $(`.active-heart#${id}`).addClass('hidden')
+          $(`.active-heart.${id}`).addClass('hidden')
           $(`.inactive-heart.${id}`).removeClass('hidden')
         }
       })
