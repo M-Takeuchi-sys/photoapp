5173b5873fe6e49db5415232f485b3da5908d1c1
masao <>
2020-10-17 21:59:15 +0900

    like.countをJQueryで操作 countの表示調整

--- app/javascript/packs/photo.js
+++ app/javascript/packs/photo.js
@@ -8,16 +8,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
   $('.inactive-heart').on('click', (e) => {
     e.preventDefault();
-    var id = $(e.currentTarget).attr('id') //クリックしたdivのid取得
+    const id = $(e.currentTarget).attr('id') //クリックしたdivのid取得
     axios.post(`/photos/${id}/like`)
       .then((response) => {
         if (response.data.status === 'ok') {
           $(`.active-heart.${id}`).removeClass('hidden')
           $(`.inactive-heart.${id}`).addClass('hidden')
-          var likeCount = $(`.like_count.${id}`).text()
-          $(`.like_count.${id}`).off()
-          var numLikeCount = parseInt(likeCount)
-          console.log(numLikeCount + 1)
+          const likeCount = $(e.currentTarget).text()
+          const numLikeCount = parseInt(likeCount)
           $(`.like_count.${id}`).text(numLikeCount + 1)
         }
       })
@@ -29,17 +27,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
   $('.active-heart').on('click', (e) => {
     e.preventDefault();
-    var id = $(e.currentTarget).attr('id') //クリックしたdivのid取得
+    const id = $(e.currentTarget).attr('id') //クリックしたdivのid取得
     axios.delete(`/photos/${id}/like`)
       .then((response) => {
         if (response.data.status === 'ok') {
           $(`.active-heart.${id}`).addClass('hidden')
           $(`.inactive-heart.${id}`).removeClass('hidden')
-          var likeCount = $(`.like_count.${id}`).text();
-          $(`.like_count.${id}`).off()
-          var numLikeCount = parseInt(likeCount)
-          console.log(numLikeCount - 1)
-          $(`.like_count.${id}`).text(numLikeCount - 1);
+          const likeCount = $(e.currentTarget).text();
+          const numLikeCount = parseInt(likeCount)
+          $(`.like_count.${id}`).text(numLikeCount - 1)
         }
       })
       .catch((e) => {
