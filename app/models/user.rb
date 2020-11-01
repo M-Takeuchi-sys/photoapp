23f8ba326361c93949917f76dc6f1e81fca443ee
masao <>
2020-11-02 06:31:01 +0900

    followとunfollow機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -50,12 +50,18 @@ class User < ApplicationRecord
   end
 
   def follow!(user)
-    following_relationships.create!(following_id: user.id)
+    user_id = get_user_id(user)
+    following_relationships.create!(following_id: user_id)
   end
 
   def unfollow!(user)
-    relation = following_relationships.find_by!(following_id: user.id)
+    user_id = get_user_id(user)
+    relation = following_relationships.find_by!(following_id: user_id)
     relation.destroy!
+  end
+
+  def has_followed?(user)
+    following_relationships.exists?(following_id: user.id)
   end
 
   def has_written?(photo) #自分が投稿した写真かどうか
@@ -64,5 +70,14 @@ class User < ApplicationRecord
 
   def has_liked?(photo)
     likes.exists?(photo_id: photo.id)
+  end
+
+  private
+  def get_user_id(user)
+    if user.is_a?(User)
+      user.id
+    else
+      user
+    end
   end
 end
