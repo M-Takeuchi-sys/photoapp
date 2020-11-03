d8397675cfbc276b7550f5c559dd9389034d47bb
masao <>
2020-11-04 08:21:28 +0900

    comment.scssの記述ミスを修正 account.idとuser.idをhamlから取得してJqueryに反映

--- app/javascript/packs/follow.js
+++ app/javascript/packs/follow.js
@@ -2,6 +2,11 @@ import $ from 'jquery'
 import axios from 'axios'
 
 document.addEventListener('DOMContentLoaded', () => {
-  
-  
-});
+  const dataset = $('#account-show').data()
+  const accountId = dataset.accountId
+  const userId = dataset.userId
+  axios.get(`/accounts/${accountId}/follows/${userId}`)
+    .then((response) => {
+      console.log(response)
+    })
+})
