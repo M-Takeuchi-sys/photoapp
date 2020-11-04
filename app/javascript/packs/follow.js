b765b3efb7319fbdb488f9a5918bcdd4f534a7bf
masao <>
2020-11-04 17:47:58 +0900

    profileページのaccount_idがnilのため修正 follow.jsのわかりやすく表示

--- app/javascript/packs/follow.js
+++ app/javascript/packs/follow.js
@@ -1,6 +1,14 @@
 import $ from 'jquery'
 import axios from 'axios'
 
+const handleFollowDisplay = (hasFollow) => {
+  if (hasFollow) {
+    $('.following').removeClass('hidden')
+  } else {
+    $('.follow').removeClass('hidden')
+  }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   const dataset = $('#account-show').data()
   const accountId = dataset.accountId
@@ -8,10 +16,6 @@ document.addEventListener('DOMContentLoaded', () => {
   axios.get(`/accounts/${accountId}/follows/${userId}`)
     .then((response) => {
       const hasFollow = response.data.hasFollow
-      if (hasFollow) {
-        $('.following').removeClass('hidden')
-      } else {
-        $('.follow').removeClass('hidden')
-      }
+      handleFollowDisplay(hasFollow)
     })
 })
