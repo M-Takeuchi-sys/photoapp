27b90f2a15793e95680b5df19b3889f007f1750f
masao <>
2020-11-08 17:59:21 +0900

    フォローしているユーザーのphotoをタイムライン表示

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -1,4 +1,9 @@
 class TimelinesController < ApplicationController
+  before_action :authenticate_user!
+
   def show
+    user_ids = current_user.followings.pluck(:id)
+    photos = Photo.where(user_id: user_ids)
+    @photos = photos.order(created_at: :desc)
   end
 end
