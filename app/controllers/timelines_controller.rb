cdea4362b631c6e4c91c6c1759d5239d67607686
masao <>
2020-11-10 11:30:40 +0900

    タイムラインページにてフォローしている人の写真 24時間以内の投稿でいいねが多い5つの写真を表示

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -3,7 +3,11 @@ class TimelinesController < ApplicationController
 
   def show
     user_ids = current_user.followings.pluck(:id)
-    photos = Photo.where(user_id: user_ids)
-    @photos = photos.order(created_at: :desc)
+    @photos = Photo.where(user_id: user_ids).order(created_at: :desc)
+
+    photos_like_count = Photo.left_joins(:likes).group(:id).order('count(likes.user_id) desc').limit(5)
+    from = Time.now - 1.day
+    to = Time.now
+    @photos_like_count = photos_like_count.where(created_at: from..to).order(created_at: :desc)
   end
 end
