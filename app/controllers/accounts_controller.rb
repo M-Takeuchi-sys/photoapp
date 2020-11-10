97e973a4f0d12a49aee4f2b7e25091cf096180bf
masao <>
2020-11-11 06:10:32 +0900

    タイムラインタグを動的classに変更 プロフィール内のphotoを作成順に変更

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -4,5 +4,7 @@ class AccountsController < ApplicationController
     if @user == current_user
       redirect_to profile_path
     end
+
+    @account_photos = @user.photos.all.order(created_at: :desc)
   end
 end
