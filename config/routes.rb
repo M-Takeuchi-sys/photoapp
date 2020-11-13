47458d048d256da293dcbe2cd0bf81d178bc376f
masao <>
2020-11-13 09:47:22 +0900

    before_actionを統一

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'photos#index'
-  resource :timeline, only: [:show]
 
   resources :photos
 
@@ -17,7 +16,10 @@ Rails.application.routes.draw do
     resources :unfollows, only: [:create]
   end
 
-  resource :profile, only: [:show, :update]
+  scope module: :apps do
+    resource :profile, only: [:show, :update]
+    resource :timeline, only: [:show]
+  end
 
   namespace :api, defaults: {format: :json} do
     scope '/photos/:photo_id' do
