4b5d5c13c20e5b6bce58f0122823984952d28666
masao <>
2020-11-18 13:44:24 +0900

    記事が保存できない ActiveStorageのvalidateにより

--- spec/requests/photos_spec.rb
+++ spec/requests/photos_spec.rb
@@ -6,8 +6,23 @@ RSpec.describe "Photos", type: :request do
 
   describe "GET /photos" do
     it "200ステータスが帰ってくる" do
-      get photos_path
+      get root_path
       expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'POST /photos' do
+    context 'ログインしている場合' do
+      before do
+        sign_in user
+      end
+
+      it '記事が保存される' do
+        photo_params = attributes_for(:photo)
+        post photos_path({photo: photo_params})
+        expect(response).to have_http_status(302)
+        expect(Photo.last.content).to eq(photo_params[:content])
+      end
     end
   end
 end
