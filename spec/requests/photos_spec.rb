e847ceb710d1718ce64f357e554e221b50e608dc
masao <>
2020-11-25 06:31:16 +0900

    if images.attached?がバリデーションに引っかかっている

--- spec/requests/photos_spec.rb
+++ spec/requests/photos_spec.rb
@@ -12,16 +12,14 @@ RSpec.describe "Photos", type: :request do
   end
 
   describe 'POST /photos' do
-    let(:params) { { photo: { content: 'content', images: images } } }
-    let(:images) { fixture_file_upload('test_image1.png') }
-
     context 'ログインしている場合' do
       before do
         sign_in user
       end
 
       it '記事が保存される' do
-        post photos_path({photo: params})
+        photo_params = attributes_for(:photo)
+        post photos_path({photo: photo_params})
         expect(response).to have_http_status(302)
         expect(Photo.last.content).to eq(photo_params[:content])
       end
